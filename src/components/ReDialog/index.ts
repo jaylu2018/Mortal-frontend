12ae542a87309d2f4eade7f95be864c2a456aa70
luyh <jaylu1995@outlook.com>
2024-03-11 11:26:25 +0800

    feat: 初始化项目信息

--- src/components/ReDialog/index.ts
+++ src/components/ReDialog/index.ts
@@ -49,11 +49,6 @@ const closeAllDialog = () => {
   dialogStore.value = [];
 };
 
-/** 千万别忘了在下面这三处引入并注册下，放心注册，不使用`addDialog`调用就不会被挂载
- * https://github.com/pure-admin/vue-pure-admin/blob/main/src/App.vue#L4
- * https://github.com/pure-admin/vue-pure-admin/blob/main/src/App.vue#L13
- * https://github.com/pure-admin/vue-pure-admin/blob/main/src/App.vue#L20
- */
 const ReDialog = withInstall(reDialog);
 
 export type { EventType, ArgsType, DialogProps, ButtonProps, DialogOptions };
